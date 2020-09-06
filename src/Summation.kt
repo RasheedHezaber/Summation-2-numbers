6997c8e5cf411fffc176e919c1d8377f73ab3887
RasheedHezaber <rasheedhezaber@gmail.com>
2020-09-06 23:22:54 +0300

    first commit

---
+++ src/Summation.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    val add = Add(5, 6)
+    println("The Sum of numbers 5 and 6 is: ${add.c}")
+}
+//primary constructor
+class Add constructor(a: Int,b:Int)
+{
+
+    var c = a+b;
+}
